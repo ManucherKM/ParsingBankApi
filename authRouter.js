67e5caef6505116548f51cf96b60cdcbbee4d0c6
ManucherKM <91463158+ManucherKM@users.noreply.github.com>
2022-06-08 13:37:56 +0500

    Add files via upload

--- authRouter.js
+++ authRouter.js
@@ -1,8 +1,9 @@
 const Router = require("express");
 const router = new Router();
-const controller = require("./authController")
+const controller = require("./authController");
 
-router.get("/allval", controller.getAllval)
-router.post("/secval", controller.postOneVal)
+router.get("/", controller.getIndex);
+router.get("/allval", controller.getAllval);
+router.post("/secval", controller.postOneVal);
 
 module.exports = router;
